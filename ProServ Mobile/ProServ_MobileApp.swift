200bf61bd855b8da6ca6a5370df414e182deb11c
ajjohnson <aj132@icloud.com>
2023-07-10 17:18:12 -0500

    Initial Commit

---
+++ ProServ Mobile/ProServ_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProServ_MobileApp.swift
+//  ProServ Mobile
+//
+//  Created by AJ Johnson on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ProServ_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
